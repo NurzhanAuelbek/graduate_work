04a3f8215cd00b6f471a1a467386ab87d425a68a
NurzhanAuelbek <82052572+NurzhanAuelbek@users.noreply.github.com>
2023-07-11 01:57:36 +0600

    Добавьте файлы проекта.

---
+++ graduate_work/Form1.Designer.cs
@@ -0,0 +1,460 @@
+﻿
+namespace graduate_work
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.шығуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.бағдарламаЖайлыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.анықтамаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.бағдарламаЖайлыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.шығуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // шығуToolStripMenuItem
+            // 
+            this.шығуToolStripMenuItem.Name = "шығуToolStripMenuItem";
+            this.шығуToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.шығуToolStripMenuItem.Text = "Шығу";
+            // 
+            // бағдарламаЖайлыToolStripMenuItem
+            // 
+            this.бағдарламаЖайлыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.анықтамаToolStripMenuItem});
+            this.бағдарламаЖайлыToolStripMenuItem.Name = "бағдарламаЖайлыToolStripMenuItem";
+            this.бағдарламаЖайлыToolStripMenuItem.Size = new System.Drawing.Size(157, 24);
+            this.бағдарламаЖайлыToolStripMenuItem.Text = "Бағдарлама жайлы";
+            // 
+            // анықтамаToolStripMenuItem
+            // 
+            this.анықтамаToolStripMenuItem.Name = "анықтамаToolStripMenuItem";
+            this.анықтамаToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.анықтамаToolStripMenuItem.Text = "Анықтама";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(322, 168);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(466, 112);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(322, 168);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Аудару";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(24, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Қазақ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(105, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Ағылшын";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(466, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Орыс";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(547, 85);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Француз";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Азербайджанский",
+            "Албанский",
+            "Английский",
+            "Арабский",
+            "Армянский",
+            "Африкаанс",
+            "Баскский",
+            "Башкирский",
+            "Белорусский",
+            "Бенгальский",
+            "Болгарский",
+            "Боснийский",
+            "Валлийский",
+            "Венгерский",
+            "Вьетнамский",
+            "Гаитянский",
+            "Галисийский",
+            "Голландский",
+            "Горномарийский",
+            "Греческий",
+            "Грузинский",
+            "Гуджарати",
+            "Датский",
+            "Иврит",
+            "Идиш",
+            "Индонезийский",
+            "Ирландский",
+            "Испанский",
+            "Итальянский",
+            "Исландский",
+            "Казахский",
+            "Каннада",
+            "Каталанский",
+            "Киргизский",
+            "Китайский",
+            "Корейский",
+            "Коса",
+            "Кхмерский",
+            "Лаосский",
+            "Латынь",
+            "Латышский",
+            "Литовский",
+            "Люксембургский",
+            "Македонский",
+            "Малагасийский",
+            "Малайский",
+            "Малаялам",
+            "Мальтийский",
+            "Маори",
+            "Маратхи",
+            "Марийский",
+            "Монгольский",
+            "Немецкий",
+            "Непальский",
+            "Нидерландский",
+            "Норвежский",
+            "Панджаби",
+            "Папьяменто",
+            "Персидский",
+            "Польский",
+            "Португальский",
+            "Румынский",
+            "Русский",
+            "Себуанский",
+            "Сербский",
+            "Сингальский",
+            "Словацкий",
+            "Словенский",
+            "Суахили",
+            "Сунданский",
+            "Тагальский",
+            "Таджикский",
+            "Тайский",
+            "Тамильский",
+            "Татарский",
+            "Телугу",
+            "Турецкий",
+            "Удмуртский",
+            "Узбекский",
+            "Украинский",
+            "Урду",
+            "Финский",
+            "Французский",
+            "Хинди",
+            "Хорватский",
+            "Чешский",
+            "Чувашский",
+            "Шведский",
+            "Шотландский (Гэльский)",
+            "Эльфийский (Синдарин)",
+            "Эмодзи",
+            "Эсперанто",
+            "Эстонский",
+            "Яванский",
+            "Якутский",
+            "Японский"});
+            this.comboBox1.Location = new System.Drawing.Point(213, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Азербайджанский",
+            "Албанский",
+            "Английский",
+            "Арабский",
+            "Армянский",
+            "Африкаанс",
+            "Баскский",
+            "Башкирский",
+            "Белорусский",
+            "Бенгальский",
+            "Болгарский",
+            "Боснийский",
+            "Валлийский",
+            "Венгерский",
+            "Вьетнамский",
+            "Гаитянский",
+            "Галисийский",
+            "Голландский",
+            "Горномарийский",
+            "Греческий",
+            "Грузинский",
+            "Гуджарати",
+            "Датский",
+            "Иврит",
+            "Идиш",
+            "Индонезийский",
+            "Ирландский",
+            "Испанский",
+            "Итальянский",
+            "Исландский",
+            "Казахский",
+            "Каннада",
+            "Каталанский",
+            "Киргизский",
+            "Китайский",
+            "Корейский",
+            "Коса",
+            "Кхмерский",
+            "Лаосский",
+            "Латынь",
+            "Латышский",
+            "Литовский",
+            "Люксембургский",
+            "Македонский",
+            "Малагасийский",
+            "Малайский",
+            "Малаялам",
+            "Мальтийский",
+            "Маори",
+            "Маратхи",
+            "Марийский",
+            "Монгольский",
+            "Немецкий",
+            "Непальский",
+            "Нидерландский",
+            "Норвежский",
+            "Панджаби",
+            "Папьяменто",
+            "Персидский",
+            "Польский",
+            "Португальский",
+            "Румынский",
+            "Русский",
+            "Себуанский",
+            "Сербский",
+            "Сингальский",
+            "Словацкий",
+            "Словенский",
+            "Суахили",
+            "Сунданский",
+            "Тагальский",
+            "Таджикский",
+            "Тайский",
+            "Тамильский",
+            "Татарский",
+            "Телугу",
+            "Турецкий",
+            "Удмуртский",
+            "Узбекский",
+            "Украинский",
+            "Урду",
+            "Финский",
+            "Французский",
+            "Хинди",
+            "Хорватский",
+            "Чешский",
+            "Чувашский",
+            "Шведский",
+            "Шотландский (Гэльский)",
+            "Эльфийский (Синдарин)",
+            "Эмодзи",
+            "Эсперанто",
+            "Эстонский",
+            "Яванский",
+            "Якутский",
+            "Японский"});
+            this.comboBox2.Location = new System.Drawing.Point(667, 78);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(357, 164);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Append Text";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.AppendTextClick);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(58, 335);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 12;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(213, 336);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Translator and application generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem шығуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem бағдарламаЖайлыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem анықтамаToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ graduate_work/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using MySql.Data.MySqlClient.Memcached;
+using MySqlX.XDevAPI;
+
+namespace graduate_work
+{
+    public partial class Form1 : Form
+    {
+        ComboBox cb;
+        BTranslator bt;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "Казахский";
+            comboBox1.SelectedItem = "Английский";
+            cb = new ComboBox();
+            bt = new BTranslator();
+            #region Коллекция языков резерного ComboBox
+            cb.Items.AddRange(new object[] {
+            "Азербайджанский",
+            "Албанский",
+            "Английский",
+            "Арабский",
+            "Армянский",
+            "Африкаанс",
+            "Баскский",
+            "Башкирский",
+            "Белорусский",
+            "Бенгальский",
+            "Болгарский",
+            "Боснийский",
+            "Валлийский",
+            "Венгерский",
+            "Вьетнамский",
+            "Гаитянский",
+            "Галисийский",
+            "Голландский",
+            "Горномарийский",
+            "Греческий",
+            "Грузинский",
+            "Гуджарати",
+            "Датский",
+            "Иврит",
+            "Идиш",
+            "Индонезийский",
+            "Ирландский",
+            "Испанский",
+            "Итальянский",
+            "Исландский",
+            "Казахский",
+            "Каннада",
+            "Каталанский",
+            "Киргизский",
+            "Китайский",
+            "Корейский",
+            "Коса",
+            "Кхмерский",
+            "Лаосский",
+            "Латынь",
+            "Латышский",
+            "Литовский",
+            "Люксембургский",
+            "Македонский",
+            "Малагасийский",
+            "Малайский",
+            "Малаялам",
+            "Мальтийский",
+            "Маори",
+            "Маратхи",
+            "Марийский",
+            "Монгольский",
+            "Немецкий",
+            "Непальский",
+            "Нидерландский",
+            "Норвежский",
+            "Панджаби",
+            "Папьяменто",
+            "Персидский",
+            "Польский",
+            "Португальский",
+            "Румынский",
+            "Русский",
+            "Себуанский",
+            "Сербский",
+            "Сингальский",
+            "Словацкий",
+            "Словенский",
+            "Суахили",
+            "Сунданский",
+            "Тагальский",
+            "Таджикский",
+            "Тайский",
+            "Тамильский",
+            "Татарский",
+            "Телугу",
+            "Турецкий",
+            "Удмуртский",
+            "Узбекский",
+            "Украинский",
+            "Урду",
+            "Финский",
+            "Французский",
+            "Хинди",
+            "Хорватский",
+            "Чешский",
+            "Чувашский",
+            "Шведский",
+            "Шотландский (Гэльский)",
+            "Эльфийский (Синдарин)",
+            "Эмодзи",
+            "Эсперанто",
+            "Эстонский",
+            "Яванский",
+            "Якутский",
+            "Японский"});
+
+            #endregion
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = "Казахский";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = "Английский";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            comboBox2.SelectedItem = "Русский";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            comboBox2.SelectedItem = "Французский";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+
+            richTextBox2.Text = bt.Translator(richTextBox1.Text, bt.GetLangPair(comboBox1.SelectedItem.ToString(), comboBox2.SelectedItem.ToString()));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cb.SelectedItem = comboBox2.SelectedItem;
+            comboBox2.SelectedItem = comboBox1.SelectedItem;
+            comboBox1.SelectedItem = cb.SelectedItem;
+        }
+        private void AppendTextClick(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                var endpoint = new Uri("https://fish-text.ru/get?format=html&number=1");
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+                richTextBox1.AppendText(json);
+                richTextBox2.Text = bt.Translator(richTextBox1.Text, bt.GetLangPair(comboBox1.SelectedItem.ToString(), comboBox2.SelectedItem.ToString()));
+            }
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            comboBox3.SelectedItem = "Русский";
+        }
+    }
+}
